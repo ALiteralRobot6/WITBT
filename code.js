bfdef2783c38076bdc1ce7a53ab4b15e53b0ca57
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-04-12 19:29:20 -0400

    Added code files

---
+++ code.js
@@ -0,0 +1,34 @@
+let scores = {};
+let wiki = '';
+let docs = ['https://thebestthing.atwebpages.com/wiki1a.txt', 'https://thebestthing.atwebpages.com/wiki1b.txt', 'https://thebestthing.atwebpages.com/wiki1c.txt'];
+let article = docs[Math.floor(Math.random()) * docs.length]; //Picks a random Wiki file.
+
+// Making a preview of the article.
+async function preview() {
+    const response = await fetch(article, {mode: 'no-cors'});
+    const data = await response.text();
+    const titles = data.split('\n'); // Splits lines.
+    wiki = titles[Math.floor(Math.random() * titles.length)]; // Picks a random Wiki title.
+    console.log(wiki);
+    document.getElementById('wikiPage').src = "https://en.wikipedia.org/wiki/" + wiki;
+}
+
+// Upvoting and downvoting.
+function upvote() {
+    if (!scores.hasOwnProperty(wiki)) {
+        scores[wiki] = 1;
+    } else {
+        scores[wiki] = scores[wiki] + 1;
+    }
+    console.log(scores);
+    preview();
+}
+
+function downvote() {
+    if (!scores.hasOwnProperty(wiki)) {
+        scores[wiki] = -1;
+    } else {
+        scores[wiki] = scores[wiki] - 1;
+    }
+    console.log(scores);
+}
