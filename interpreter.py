ed744067151feb49f2198dc3d037ebcb574053c2
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-04-13 16:43:24 -0400

    Python test for files

--- interpreter.py
+++ interpreter.py
@@ -1,12 +1,3 @@
-from flask import Flask, request, jsonify
-
-app = Flask(__name__)
-
-@app.route('/receive_dictionary', methods=['POST'])
-def receive_dictionary():
-    data = request.get_json()
-    print(data)  # Prints the received dictionary
-    return jsonify({'message': 'Received dictionary'}), 200
-
-if __name__ == '__main__':
-    app.run(debug=True)
+data = open('pollingData.txt')
+print(data.read())
+close('pollingData.txt')
