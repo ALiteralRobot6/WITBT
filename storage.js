c52069f7e8d4d02b7c3bbb9e44ad7a990453db43
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-04-13 20:48:33 -0400

    Delete storage.js

--- storage.js
+++
@@ -1,6 +0,0 @@
-import {scores} from './code.js';
-let dictString = JSON.stringify(scores);
-fs.writeFile('pollingData', dictString, (err) => {
-if (err) throw err;
-console.log('Dictionary written to file');
-});
