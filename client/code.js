b65d697958fa441f0aabac883c5c49a67ad5cd36
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-05-27 18:57:56 -0400

    Update code.js

--- client/code.js
+++ client/code.js
@@ -7,9 +7,8 @@ preview()
 async function preview() {
     let docs = ['wiki1a.txt', 'wiki1b.txt', 'wiki1c.txt', 'wiki2.txt', 'wiki3.txt', 'wiki4.txt', 'wiki5.txt', 'wiki6.txt', 'wiki7.txt', 'wiki8.txt', 'wiki9.txt', 'wiki10.txt', 'wiki11.txt', 'wiki12.txt', 'wiki13.txt', 'wiki14.txt', 'wiki15.txt', 'wiki16.txt', 'wiki17.txt', 'wiki18.txt', 'wiki19.txt', 'wiki20.txt', 'wiki20.txt', 'wiki21.txt', 'wiki22.txt', 'wiki23.txt', 'wiki24.txt'];
     let article = docs[Math.floor(Math.random()) * docs.length]; //Picks a random Wiki file.
-    const response = await fetch(article, {mode: 'no-cors'});
-    const data = await response.text();
-    const titles = data.split('\n'); // Splits lines.
+    console.log(article);
+    const titles = article.split('\n'); // Splits lines.
     wiki = titles[Math.floor(Math.random() * titles.length)]; // Picks a random Wiki title.
     console.log(wiki);
     document.getElementById('wikiPage').src = "https://en.wikipedia.org/wiki/" + wiki;
