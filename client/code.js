d530e503d40739e35f46d1dce55b9ea9dff9a7e1
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-06-03 18:05:55 -0400

    Update code.js

--- client/code.js
+++ client/code.js
@@ -7,7 +7,7 @@ preview()
 async function preview() {
     let docs = ['client/wiki1a.txt', 'client/wiki1b.txt', 'client/wiki1c.txt', 'client/wiki2.txt', 'client/wiki3.txt', 'client/wiki4.txt', 'client/wiki5.txt', 'client/wiki6.txt', 'client/wiki7.txt', 'client/wiki8.txt', 'client/wiki9.txt', 'client/wiki10.txt', 'client/wiki11.txt', 'client/wiki12.txt', 'client/wiki13.txt', 'client/wiki14.txt', 'client/wiki15.txt', 'client/wiki16.txt', 'client/wiki17.txt', 'client/wiki18.txt', 'client/wiki19.txt', 'client/wiki20.txt', 'client/wiki20.txt', 'client/wiki21.txt', 'client/wiki22.txt', 'client/wiki23.txt', 'client/wiki24.txt'];
     console.log(docs);    
-    console.log(docs[6]);
+    console.log(docs[Math.random() * 28]);
 }
 
 // Upvoting and downvoting.
