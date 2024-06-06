aa0ad3da411e2c29583a4af248799db5a750ed7f
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-06-06 16:51:38 -0400

    Update code.js

--- client/code.js
+++ client/code.js
@@ -6,8 +6,9 @@ preview()
 // Making a preview of the article.
 async function preview() {
     let docs = ['client/wiki1a.txt', 'client/wiki1b.txt', 'client/wiki1c.txt', 'client/wiki2.txt', 'client/wiki3.txt', 'client/wiki4.txt', 'client/wiki5.txt', 'client/wiki6.txt', 'client/wiki7.txt', 'client/wiki8.txt', 'client/wiki9.txt', 'client/wiki10.txt', 'client/wiki11.txt', 'client/wiki12.txt', 'client/wiki13.txt', 'client/wiki14.txt', 'client/wiki15.txt', 'client/wiki16.txt', 'client/wiki17.txt', 'client/wiki18.txt', 'client/wiki19.txt', 'client/wiki20.txt', 'client/wiki20.txt', 'client/wiki21.txt', 'client/wiki22.txt', 'client/wiki23.txt', 'client/wiki24.txt'];
-    console.log(docs);    
-    console.log(docs[Math.floor(Math.random() * docs.length]));
+    console.log(docs);
+    let file = docs[Math.floor(Math.random() * docs.length]);
+    console.log(file);
 }
 
 // Upvoting and downvoting.
