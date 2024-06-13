76223b5e3b20f14ed3d3711be6d640ea882b768f
ALiteralRobot6 <165726417+ALiteralRobot6@users.noreply.github.com>
2024-06-12 20:21:19 -0400

    Update code.js

--- client/code.js
+++ client/code.js
@@ -8,7 +8,8 @@ async function preview() {
     // The following is for testing purposes.
     let docs = ['client/wiki1a.txt', 'client/wiki1b.txt', 'client/wiki1c.txt', 'client/wiki2.txt', 'client/wiki3.txt', 'client/wiki4.txt', 'client/wiki5.txt', 'client/wiki6.txt', 'client/wiki7.txt', 'client/wiki8.txt', 'client/wiki9.txt', 'client/wiki10.txt', 'client/wiki11.txt', 'client/wiki12.txt', 'client/wiki13.txt', 'client/wiki14.txt', 'client/wiki15.txt', 'client/wiki16.txt', 'client/wiki17.txt', 'client/wiki18.txt', 'client/wiki19.txt', 'client/wiki20.txt', 'client/wiki20.txt', 'client/wiki21.txt', 'client/wiki22.txt', 'client/wiki23.txt', 'client/wiki24.txt'];
     console.log(docs);
-    const response = await fetch("https://aliteralrobot6.github.io/WITBT/client/" + docs);
+    let wikiFile = docs[Math.floor(Math.random() * docs.length)];
+    const response = await fetch("https://aliteralrobot6.github.io/WITBT/client/" + wikiFile);
     const text = await response.text();
     console.log(text);
 
